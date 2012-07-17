ab9bf132e4aa87e08011a7826caeac9f35e06703
dmueller2001@gmail.com <dmueller2001@gmail.com>
2012-07-17 22:03:55 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -9,30 +9,30 @@ var getPlacemarks = function (params, callback) {
   // Push some more closeby points onto the array
   //my Vancouver Edition list
 points.push({lat: 49.1943167455, lon: -123.179767288, title: 'Airport Totems'});
-points.push({lat: lat 49.299353, lon: lon -123.121044, title: 'Stanley Park'});
-points.push({lat: lat 49.266569, lon: lon -123.099169, title: 'Burrard Bridge'});
-points.push({lat: lat 49.287894, lon: lon -123.113692, title: 'Canada Place'});
-points.push({lat: lat 49.26505, lon: lon -123.114486, title: 'Canadian Tire on Cambie'});
-points.push({lat: lat 49.343708, lon: lon -123.112897, title: 'Capilano Suspension Bridge'});
-points.push({lat: lat 49.301867, lon: lon -122.955133, title: 'Cates Park, Deep Cove'});
-points.push({lat: lat 49.37395, lon: lon -123.272981, title: 'Horseshoe Bay Ferry Terminal'});
-points.push({lat: lat 49.375106, lon: lon -123.275267, title: 'Horseshoe Bay Park'});
-points.push({lat: lat 49.249625, lon: lon -123.095836, title: 'Jomar Lanot Memorial Pole'});
-points.push({lat: lat 49.269339, lon: lon -123.260689, title: 'Museum of Anthropology at UBC'});
-points.push({lat: lat 49.282361, lon: lon -123.094147, title: 'Oppenheimer Park'});
-points.push({lat: lat 49.278572, lon: lon -123.120892, title: 'Seymour Street'});
-points.push({lat: lat 49.319883, lon: lon -123.1262, title: 'Squamish Nation, North Vancouver'});
-points.push({lat: lat 49.266569, lon: lon -123.099169, title: 'Wil Sayt Bakwhlgat & Longhouse (NEC)'});
-points.push({lat: lat 49.35835, lon: lon -123.267, title: 'Thunderbird Marina'});
-points.push({lat: lat 49.268483, lon: lon -123.252483, title: 'Thunderbird Totem Pole'});
-points.push({lat: lat 49.322797, lon: lon -123.113275, title: 'Tomahawk Cafe Totem Poles'});
-points.push({lat: lat 49.23865, lon: lon -123.129617, title: 'Van Dusen Gardens'});
-points.push({lat: lat 49.194433, lon: lon -123.176008, title: 'Vancouver Airport'});
-points.push({lat: lat 49.2771, lon: lon -123.147683, title: 'Vanier Park'});
-points.push({lat: lat 49.322983, lon: lon -123.150567, title: 'Welcome Guardian Woman '});
-points.push({lat: lat 49.35755, lon: lon -123.271547, title: 'West Vancouver Yacht Club'});
-points.push({lat: lat 49.26355, lon: lon -123.254083, title: 'Whaler Pole'});
-points.push({lat: lat 49.322797, lon: lon -123.113275, title: 'Tomahawk Cafe Poles'});
+points.push({lat: 49.299353, lon:  -123.121044, title: 'Stanley Park'});
+points.push({lat: 49.266569, lon:  -123.099169, title: 'Burrard Bridge'});
+points.push({lat: 49.287894, lon: -123.113692, title: 'Canada Place'});
+points.push({lat: 49.26505, lon:  -123.114486, title: 'Canadian Tire on Cambie'});
+points.push({lat: 49.343708, lon: -123.112897, title: 'Capilano Suspension Bridge'});
+points.push({lat: 49.301867, lon: -122.955133, title: 'Cates Park, Deep Cove'});
+points.push({lat: 49.37395, lon: -123.272981, title: 'Horseshoe Bay Ferry Terminal'});
+points.push({lat: 49.375106, lon: -123.275267, title: 'Horseshoe Bay Park'});
+points.push({lat: 49.249625, lon: -123.095836, title: 'Jomar Lanot Memorial Pole'});
+points.push({lat: 49.269339, lon: -123.260689, title: 'Museum of Anthropology at UBC'});
+points.push({lat: 49.282361, lon: -123.094147, title: 'Oppenheimer Park'});
+points.push({lat: 49.278572, lon: -123.120892, title: 'Seymour Street'});
+points.push({lat: 49.319883, lon: -123.1262, title: 'Squamish Nation, North Vancouver'});
+points.push({lat: 49.266569, lon: -123.099169, title: 'Wil Sayt Bakwhlgat & Longhouse (NEC)'});
+points.push({lat: 49.35835, lon: -123.267, title: 'Thunderbird Marina'});
+points.push({lat: 49.268483, lon: -123.252483, title: 'Thunderbird Totem Pole'});
+points.push({lat: 49.322797, lon: -123.113275, title: 'Tomahawk Cafe Totem Poles'});
+points.push({lat: 49.23865, lon: -123.129617, title: 'Van Dusen Gardens'});
+points.push({lat: 49.194433, lon: -123.176008, title: 'Vancouver Airport'});
+points.push({lat: 49.2771, lon: -123.147683, title: 'Vanier Park'});
+points.push({lat: 49.322983, lon: -123.150567, title: 'Welcome Guardian Woman '});
+points.push({lat: 49.35755, lon: -123.271547, title: 'West Vancouver Yacht Club'});
+points.push({lat: 49.26355, lon: -123.254083, title: 'Whaler Pole'});
+points.push({lat: 49.322797, lon: -123.113275, title: 'Tomahawk Cafe Poles'});
   //end of my list
   
   return callback(null, {points: points});
